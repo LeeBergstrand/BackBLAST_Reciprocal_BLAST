861e6df54c9c82001138854ec627a67325ab3845
Lee Bergstrand <leemacboy@gmail.com>
2014-02-24 16:17:30 -0800

    Now writes to file.

--- BackBLAST.py
+++ BackBLAST.py
@@ -20,6 +20,7 @@ import sys
 import csv
 import subprocess
 from Bio import SeqIO
+import cProfile
 #===========================================================================================================
 # Functions:
 
@@ -40,14 +41,12 @@ def runBLAST(query, BLASTDBFile):
 	
 	# If the string starts with > it is a fasta file and should be sent to blast via stdin.
 	if query.startswith(">"):
-		print "Back Blasting Hit Genome to Query Genome..."
 		BlastArgs = "blastp -db "+ BLASTDBFile + " -num_threads 16 -outfmt \"10 qseqid sseqid pident evalue qcovhsp score\""
 		blastp = subprocess.Popen(BlastArgs, shell=True, stdin=subprocess.PIPE,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		BLASTOut, err = blastp.communicate(query)
 	
 	# Else the string is a file name and should be put in the file argument.
 	else:
-		print "Forward Blasting to subject Genome..."
 		BLASTOut = subprocess.check_output(["blastp", "-db", BLASTDBFile, "-query", query, "-evalue", "1e-40", "-num_threads", "16", "-outfmt", "10 qseqid sseqid pident evalue qcovhsp score"]) # Runs BLASTp and save output to a string. Blastp is set to output xml which can be parsed.
 	return BLASTOut
 	
@@ -79,14 +78,11 @@ def getTopHits(BLASTCSVOut):
 	topHits.append(BLASTCSVOut[0])	
 	currentQuery = BLASTCSVOut[0]
 	
-	# Looks for first occurnce of each query accession. This should be the top hit. 
-	# If ties occur includes these tied in the top hit list. (Ties should have the same score)
+	# If ties occur include these tied in the top hit list. (Ties should have the same score)
 	for x in range(1, len(BLASTCSVOut)):
-		if BLASTCSVOut[x][0] != currentQuery[0]:
-			topHits.append(BLASTCSVOut[x])
-			currentQuery = BLASTCSVOut[x]
-		elif BLASTCSVOut[x][5] == currentQuery[5]: # Untested Jesus take the wheel...
-			topHits.append(BLASTCSVOut[x])
+		if BLASTCSVOut[x][5] == currentQuery[5]:			topHits.append(BLASTCSVOut[x])
+		else:
+			break # Break out of loop if hit has lower score than top hit. # Should remove break as it is bad voodoo...
 
 	return topHits
 #-------------------------------------------------------------------------------------------------
@@ -96,7 +92,7 @@ def getQueryGenome(GenomesCSV, queryProtein):
 	for row in GenomesCSV:
 		if row[1].strip() == queryProtein.strip(): 
 			GenomeAccession = row[0]
-			break # Should remove break as its bad voodoo...
+			break # Should remove break as it is bad voodoo...
 	return GenomeAccession
 #===========================================================================================================
 # Main program code:
@@ -116,9 +112,9 @@ if not queryGenomesFile.endswith(".csv"):
 	
 BLASTDBFile = sys.argv[3]
 
+print "Forward Blasting to subject Genome..."
 BLASTOut = runBLAST(queryFile, BLASTDBFile) # Forward BLASTs from query protien to subject genome
 BLASTOut = filtreBLASTCSV(BLASTOut, 30) # Filtres BLAST results by PIdnet.
-Hits = getTopHits(BLASTOut) # Finds top hits for each query protein and astors them in a list.
 
 # Attemps to open csv contain a csv that links each query protien to its respective genome. 
 try:
@@ -131,9 +127,11 @@ except IOError:
 	print "Failed to open " + queryGenomesFile
 	exit(1)
 
+BackBlastOutput = []
+
+print "Back Blasting Hits to Query Genome..."
 # For each top Hit...
-for hit in Hits:
-	print "--------------------------------------------------------------------"
+for hit in BLASTOut:
 	subjectProtein = hit[1]
 	queryProtein = hit[0]
 	
@@ -156,13 +154,29 @@ for hit in Hits:
 	BackBlastOut = runBLAST(subjectProtienFASTA, CurrentQueryGenome)
 	BackBlastOut = filtreBLASTCSV(BackBlastOut, 30) # Filtres BLAST results by PIdnet.
 	BackHits = getTopHits(BackBlastOut) # Little bit sledge hammery since it goes through all of blast results. 										 # Only first couple of hits will be relevant. 
+	match = False # By default set match to false
 	for BackHit in BackHits:
-		print hit
-		print BackHit
 		if BackHit[1] == queryProtein:
 			match = True
-		else:
-			match = False
 			
-		print "Is it a match? ", match
+	if match == True:
+		BackBlastOutput.append(hit)
+
+		
+OutFile = BLASTDBFile.rstrip(".faa") + ".csv"
+
+# Attempts to write reciprical BLAST output to file.
+try:
+	writeFile = open(OutFile, "w") 	
+	writer = csv.writer(writeFile)
+	print ">> Output file created."
+	print ">> Writing Data..."
+except IOError:
+	print "Failed to create " + outFile
+	exit(1)
 	
+for row in BackBlastOutput:
+	writer.writerow(row)
+
+writeFile.close()
+print "done"	
