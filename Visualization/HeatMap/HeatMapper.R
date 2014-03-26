e834fbf586e4312b355897c6049d2b679ff0b5da
Lee Bergstrand <leemacboy@gmail.com>
2014-03-26 11:13:14 -0700

    WE GOT HEATMAPS

--- Visualization/HeatMap/HeatMapper.R
+++ Visualization/HeatMap/HeatMapper.R
@@ -1,12 +1,16 @@
 #!/usr/bin/env Rscript 
-# Created by: Lee Bergstrand & Erick Cardenas Poire 
+# Created by: Lee Bergstrand
 # Descript: Converts BLAST TotalBLASTResultss from BackBlast into a heatmap.  
 #             
 # Requirements: - reshape2 and RColorBrewer modules
 #----------------------------------------------------------------------------------------
-options(warn = 2) 
+library(reshape2)
+library(RColorBrewer)
+library(ape)
+
 # Sets the working directory. 
-setwd("/Users/lee/Dropbox/R & D/Repositories/BackBLAST-Gene-Cluster-Finder/Visualization/HeatMap/TestData")
+setwd("/Users/lee/Dropbox/RandD/Repositories/BackBLAST-Gene-Cluster-Finder/Visualization/HeatMap/TestData")
+tree = read.tree("/Users/lee/Dropbox/RandD/Repositories/BackBLAST-Gene-Cluster-Finder/Visualization/HeatMap/SteriodBacteria16SAligned2.nwk")
 
 # Gets a list files from the working directory.
 fileList = list.files(path = getwd(), all.files = FALSE, pattern = "\\.csv$") 
@@ -27,36 +31,29 @@ while (fileCounter <= length(fileList)){
 }
 colnames(TotalBLASTResults) = c("QuerySeqID", "SubjectSeqID", "PercentIdent", "Evalue", "QueryCoverage", "Bitscore", "TargetOrganism")
 
-library(reshape2)
-library(RColorBrewer)
-library(ape)
-
 #check this function help to see what to do when multiple values occur
 HeatmapMatrix = acast(TotalBLASTResults, QuerySeqID ~ TargetOrganism, value.var = "PercentIdent") # Converts TotalBLASTResults to wide format data matrix.
 HeatmapMatrix[is.na(HeatmapMatrix)] = 0 # Replaces NA values with zero for heatmap function.
 
-tree = read.tree("/Users/lee/Desktop/Steriod16S.nwk")
-
+# The Tree needs to be rooted so get the user to interactivly pick a root.
 print("Please select a root.")
 plot(tree)
-
 tree = root(tree, resolve.root = TRUE, interactive = TRUE)
-
 print("Thanks, continuing...")
-plot(tree)
 
+# The tree must be ultrametric to be able to be passed to hclust.
 tree$edge.length[which(tree$edge.length == 0)] = 0.00001
 tree <- chronopl(tree, lambda = 0.1, tol = 0)
-plot(tree)
 
 dendrogram = as.dendrogram(as.hclust.phylo(tree))
-plot(dendrogram)
 
 cladeOrder = order.dendrogram(dendrogram)
 cladeName =  labels(dendrogram)
-cladePosition = data.frame(cladeName, cladeOrder)
-print(cladePosition)
-cladePosition = cladePosition[order(cladePosition$cladeOrder),]
-print(cladePosition)
 
-heatmap(HeatmapMatrix, col = brewer.pal(8,"Blues"), breaks = c(0,30,40,50,60,70,80,90,100), Rowv = NA, Colv = NA)
+# Rearanges matrix to match tip ordering of the dendrogram.
+cladePosition = data.frame(cladeName, cladeOrder)
+cladePosition = cladePosition[order(cladePosition$cladeOrder),]
+newOrder = match(cladePosition$cladeName, colnames(HeatmapMatrix))
+OrderedHeatmapMatrix = HeatmapMatrix[,newOrder]
+
+heatmap(OrderedHeatmapMatrix, col = brewer.pal(8,"Greens"), breaks = c(0,30,40,50,60,70,80,90,100), Rowv = NA, Colv = dendrogram)
