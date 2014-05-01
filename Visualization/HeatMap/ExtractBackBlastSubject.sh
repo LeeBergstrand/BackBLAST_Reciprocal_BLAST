24fad09f9b1f96b14e2712c3fe6f0886c54e9367
Lee Bergstrand <leemacboy@gmail.com>
2014-05-01 10:20:11 -0700

    Update extraction shell script.

--- Visualization/HeatMap/ExtractBackBlastSubject.sh
+++ Visualization/HeatMap/ExtractBackBlastSubject.sh
@@ -4,7 +4,7 @@
 for file;
 do
 	echo Etracting subjects from $file
-	python ExtractBackBlastSubject.py $file /Users/lee/Dropbox/RandD/Repositories/BackBLAST-Gene-Cluster-Finder/Visualization/HeatMap/TestData/SecondRun/CompleteResults.csv
+	python /Users/lee/Dropbox/RandD/Repositories/BackBLAST-Gene-Cluster-Finder/Visualization/HeatMap/ExtractBackBlastSubject.py $file /Users/lee/Dropbox/RandD/Repositories/BackBLAST-Gene-Cluster-Finder/Visualization/HeatMap/TestData/SecondRun/CompleteResults.csv
 	grep "_" "$file.out" > "$file.prod"
 	grep -v "_" "$file.out" > "$file.ncbi"
 done
