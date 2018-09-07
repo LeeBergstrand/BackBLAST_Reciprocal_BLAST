482dfc058990aa6d081f37e7104f5ba9452ea34f
Jackson M. Tsuji <jackson.tsuji@uwaterloo.ca>
2018-09-07 13:15:20 -0400

    Updated RemoveDuplicates.sh to run on individual CSV files and output to STDOUT

--- Visualization/RemoveDuplicates.sh
+++ Visualization/RemoveDuplicates.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
-# A simple script that removes multiple hits from BackBlast results.
+set -euo pipefail
+# Copyright Lee H. Bergstrand and Jackson M. Tsuji, 2018
+# A simple script that removes multiple hits from BackBLAST results.
 
-for csv;
-do
-	echo Removing duplicate hits from on ${csv}
-	sort -k 1,1 -t , -u ${csv} > "$csv.out"
-done
-echo Done.
+script_name=${0##*/}
+
+# If input field is empty, give error message and end script
+if [ $# == 0 ]; then
+    printf "${script_name}: A simple script that removes multiple hits from BackBLAST results.\n"
+    printf "Usage: ${script_name} input.csv > output.csv\n\n"
+    exit 1
+fi
+
+# Get input from user
+input=$1
+
+# Note: (>&2 ... ) prints to STDERR
+(>&2 echo "[$(date -u)] Removing duplicate hits from ${input}")
+
+# Prints output to STDOUT
+sort -k 1,1 -t , -u ${input}
+
+(>&2 echo "[$(date -u)] ${script_name}: done.")
+
 exit 0
