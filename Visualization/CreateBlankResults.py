808957e054be6cfb244b43b1f2cfad16df00772e
Jackson M. Tsuji <jackson.tsuji@uwaterloo.ca>
2018-09-29 18:17:06 -0400

    Added logger

--- Visualization/CreateBlankResults.py
+++ Visualization/CreateBlankResults.py
@@ -14,7 +14,8 @@
 # Imports:
 import sys
 import os
-import datetime
+import logging
+import time
 
 from Bio import SeqIO
 from shutil import copyfile
@@ -31,16 +32,6 @@ def argsCheck(numArgs):
         print("Copyright Lee H. Bergstrand and Jackson M. Tsuji, 2018\n")
         print("Usage: " + sys.argv[0] + "  <blast_output_file.csv> <query_proteins.faa> <new_blast_output_file.csv>")
         sys.exit(1)
-
-
-# Gives a timestamp (string) for help with logging
-def ts():
-    current_time = datetime.datetime.utcnow()
-    time_formatted = current_time.strftime("%a %b %d %H:%M:%S UTC %Y")
-
-    timestamp = "[ " + time_formatted + " ]: "
-
-    return(timestamp)
 
 
 # Checks whether or not the provided input_csv is an empty file. Returns logical.
@@ -60,7 +51,7 @@ def file_extension_check(filename, extension):
     # Throws a warning if file extension does not appear to be appropriate
 
     if not filename.endswith('.' + extension):
-        print(ts() + "WARNING: '" + filename + "' may not be a '" + extension + "' file!")
+        logger.warning("'" + filename + "' may not be a '" + extension + "' file!")
 
 
 # Uses input query_proteins FAA file to build a blank BLAST CSV table. Returns the table.
@@ -77,7 +68,7 @@ def generate_blank_results(query_proteins):
         handle.close()
 
     except IOError:
-        print(ts() + "Failed to open " + query_proteins)
+        logger.error("Failed to open " + query_proteins)
         sys.exit(1)
 
     blank_results = "\n".join(blank_results_list)
@@ -94,24 +85,29 @@ def write_blank_results(blank_results, new_blast_file):
         writeFile.close()
 
     except IOError:
-        print(ts() + "Failed to create " + new_blast_file)
+        logger.error("Failed to create " + new_blast_file)
         sys.exit(1)
 
 
 def main(args):
-    # Housekeeping
-    argsCheck(4)  # Checks if the number of arguments are correct.
+    # Set up the logger
+    logging.basicConfig(level=logging.DEBUG, format="[ %(asctime)s UTC ]: %(levelname)s: %(module)s: %(message)s")
+    logging.Formatter.converter = time.gmtime
+    logger = logging.getLogger(__name__)  # Not sure if this is needed
+
+    # Check if the number of arguments are correct.
+    argsCheck(4)
 
     # Get user-provided variables
     original_blast_file = sys.argv[1]
     query_proteins = sys.argv[2]
     new_blast_file = sys.argv[3]
 
-    # Print settings
-    print(ts() + "Running "+ sys.argv[0])
-    print(ts() + "Settings: Input BLAST CSV table: " + original_blast_file)
-    print(ts() + "Settings: Query protein FastA file: " + query_proteins)
-    print(ts() + "Settings: Output BLAST CSV table: " + new_blast_file)
+    # Report the input arguments
+    logger.debug("Running " + os.path.basename(sys.argv[0]))
+    logger.debug("Settings: Input BLAST CSV table: " + original_blast_file)
+    logger.debug("Settings: Query protein FastA file: " + query_proteins)
+    logger.debug("Settings: Output BLAST CSV table: " + new_blast_file)
 
     # Check file extensions
     file_extension_check(original_blast_file, "csv")
@@ -119,25 +115,25 @@ def main(args):
     file_extension_check(new_blast_file, "csv")
 
     # If the CSV file is not empty, then just keep the file as is.
-    if check_if_input_CSV_is_empty(original_blast_file) == False:
+    if check_if_input_csv_is_empty(original_blast_file) == False:
 
-        print(ts() + "Provided BLAST file has content - no need to replace. Copying to output file.")
+        logger.debug("Provided BLAST file has content - no need to replace. Copying to output file.")
         copyfile(original_blast_file, new_blast_file)
 
     # If it is empty, then make a fake file.
-    elif check_if_input_CSV_is_empty(original_blast_file) == True:
+    elif check_if_input_csv_is_empty(original_blast_file) == True:
 
         # Stores file one for input checking.
-        print(ts() + "Generating blank BLAST table based on FAA file provided")
+        logger.debug("Generating blank BLAST table based on FAA file provided")
         blank_results = generate_blank_results(query_proteins)
 
-        print(ts() + "Writing to CSV file '" + new_blast_file + "'")
+        logger.debug("Writing to CSV file '" + new_blast_file + "'")
         write_blank_results(blank_results, new_blast_file)
 
     else:
-        print(ts() + "Something went wrong. Exiting...")
+        logger.error("Something went wrong. Exiting...")
         sys.exit(1)
 
-    print(ts() + "Done")
+    logger.debug("Done")
 
 main(sys.argv)
