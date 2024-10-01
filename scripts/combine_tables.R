5064527e2f942a0598d440782f9fd45f0de01670
Jackson Tsuji <jackson.tsuji@jamstec.go.jp>
2024-10-01 16:59:09 +0900

    Bump year and version

--- scripts/combine_tables.R
+++ scripts/combine_tables.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 # combine_tables.R
-# Copyright Lee H. Bergstrand and Jackson M. Tsuji, 2022
+# Copyright Lee H. Bergstrand and Jackson M. Tsuji, 2024
 # Combines BLAST output (CSV) tables into a single output table with headers
 # Part of the BackBLAST pipeline
 
